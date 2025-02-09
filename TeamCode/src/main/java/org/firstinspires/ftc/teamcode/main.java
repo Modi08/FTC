c8db0ae59b95cb9af69e9aada749c8c2962ca64f
Ekansh <indiabestgo@gmail.com>
2025-02-09 14:08:43 +0100

    Made the main mode work for the new robot design with the sliders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
@@ -15,17 +16,18 @@ public class main extends LinearOpMode {
     public DcMotor leftDrive;
     public DcMotor rightDrive;
     public DcMotor armMotor;
+    public DcMotor leftSlider;
+    public DcMotor rightSlider;
+    public DcMotor topSlider;
     public CRServo intake;
     public Servo wrist;
+    public Servo basket;
 
     // Can be used convert degrees to ticks
     final double armTicksPerDegree = 28 * 250047.0 / 4913.0 * 100.0 / 20.0 * 1/360.0;
 
     //Arm position Constants
     final double armClosedRobot = 0;
-    final double armCollect = 250 * armTicksPerDegree;
-    final double armClearBarrier = 230 * armTicksPerDegree;
-    final double armScoreSpecOrSample = 160 * armTicksPerDegree;
     final double armHangHook = 120 * armTicksPerDegree;
     final double armWinchRobot = 15 * armTicksPerDegree;
 
@@ -38,6 +40,10 @@ public class main extends LinearOpMode {
     final double wristFoldedIn = 0.4;
     final double wristFoldedOut = 0.065;
 
+    //Basket position constants
+    final double basketStore = 0;
+    final double basketScore = 0.5;
+
     //TODO Understand the following
     final double FudgeFactor = 15 * armTicksPerDegree;
 
@@ -47,21 +53,22 @@ public class main extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        //Drive train Variables
-        double leftPower;
-        double rightPower;
-        double forward;
-        double rotate;
-        double max;
-
         //Initializing Motors
         leftDrive = hardwareMap.get(DcMotor.class, "motorLeft");
         rightDrive = hardwareMap.get(DcMotor.class, "motorRight");
         armMotor = hardwareMap.get(DcMotor.class, "motorArm");
+        leftSlider = hardwareMap.get(DcMotor.class, "sliderLeft");
+        rightSlider = hardwareMap.get(DcMotor.class, "sliderRight");
+        topSlider = hardwareMap.get(DcMotor.class, "sliderTop");
 
         //Configuring DriveTrain
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftSlider.setDirection(DcMotor.Direction.FORWARD);
+        rightSlider.setDirection(DcMotor.Direction.FORWARD);
+        topSlider.setDirection(DcMotor.Direction.FORWARD);
+
 
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -69,6 +76,9 @@ public class main extends LinearOpMode {
 
         //Safety ALERT
         ((DcMotorEx) armMotor).setCurrentAlert(5, CurrentUnit.AMPS);
+        ((DcMotorEx) leftSlider).setCurrentAlert(5, CurrentUnit.AMPS);
+        ((DcMotorEx) rightSlider).setCurrentAlert(5, CurrentUnit.AMPS);
+        ((DcMotorEx) topSlider).setCurrentAlert(5, CurrentUnit.AMPS);
 
         //Configuring Arm
         armMotor.setTargetPosition(0);
@@ -78,6 +88,7 @@ public class main extends LinearOpMode {
         //Initializing Servos
         intake = hardwareMap.get(CRServo.class, "Servo1");
         wrist = hardwareMap.get(Servo.class, "wrist");
+        basket = hardwareMap.get(Servo.class, "basket");
 
         //Configuring Servos
         intake.setPower(intakeOff);
@@ -89,81 +100,32 @@ public class main extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            //Getting joystick inputs
-            forward = -gamepad1.left_stick_y;
-            rotate = gamepad1.right_stick_x;
+            controlDriveMotors();
 
-            //Calculate Power to wheels
-            leftPower = (forward + rotate) * 0.9;
-            rightPower = (forward - rotate) * 0.9;
-            max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
-            if (max > 1.0)
-            {
-                leftPower /= max;
-                rightPower /= max;
+            controlHorizontalSliders();
+
+            controlVerticalSlider();
+            //Retract top slider
+            if (gamepad2.b) {
+                retractVerticalSlider();
             }
-
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
-
-            //Toggling Intake Power
+            //Collect Sample
             if (gamepad1.a) {
-                intake.setPower(intakeCollect);
+                collectSample();
             }
-            else if (gamepad1.x) {
-                intake.setPower(intakeOff);
-            }
-            else if (gamepad1.b) {
-                intake.setPower(intakeDeposit);
-            }
-
-            if (gamepad1.y) {
-                if (wrist.getPosition() == wristFoldedIn) {
-                    wrist.setPosition(wristFoldedOut);
-                } else {
-                    wrist.setPosition(wristFoldedIn);
-                }
-            }
-
-
 
             //Toggling Arm Position
-            if(gamepad2.right_bumper){
-                armPosition = armCollect;
-                wrist.setPosition(wristFoldedOut);
-                intake.setPower(intakeCollect);
-            }
-
-            else if (gamepad2.left_bumper){
-                armPosition = armClearBarrier;
-            }
-
-            else if (gamepad2.dpad_left) {
-                armPosition = armClosedRobot;
-                intake.setPower(intakeOff);
-                wrist.setPosition(wristFoldedIn);
-            }
-
-            else if (gamepad2.dpad_right){
-                armPosition = armScoreSpecOrSample;
-                wrist.setPosition(wristFoldedOut);
-            }
-
-            else if (gamepad2.dpad_up){
+            if (gamepad2.dpad_up){
                 armPosition = armHangHook;
                 intake.setPower(intakeOff);
                 wrist.setPosition(wristFoldedIn);
-            }
-
-            else if (gamepad2.dpad_down){
+            } else if (gamepad2.dpad_down){
                 armPosition = armWinchRobot;
                 intake.setPower(intakeOff);
                 wrist.setPosition(wristFoldedIn);
             }
 
             //Safety Precautions
-            armPositionFudgeFactor = FudgeFactor * (gamepad1.right_trigger + (-gamepad1.left_trigger));
-
             armMotor.setTargetPosition((int) (armPosition + armPositionFudgeFactor));
 
             ((DcMotorEx) armMotor).setVelocity(2100);
@@ -180,6 +142,68 @@ public class main extends LinearOpMode {
             telemetry.addData("arm Encoder: ", armMotor.getCurrentPosition());
             telemetry.update();
 
+        }
+    }
+
+    private void collectSample() {
+        boolean run = true;
+        while (run) {
+            wrist.setPosition(wristFoldedOut);
+            intake.setPower(intakeCollect);
+            if (gamepad1.a) {
+                run = false;
+                intake.setPower(intakeOff);
+                wrist.setPosition(wristFoldedIn);
+            }   
+        }
+
+        leftSlider.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightSlider.setDirection(DcMotorSimple.Direction.REVERSE);
+        while (!((DcMotorEx) leftSlider).isOverCurrent() || !((DcMotorEx) rightSlider).isOverCurrent()) {
+            leftSlider.setPower(1);
+            rightSlider.setPower(1);
+        }
+        
+    }
+
+    private void controlDriveMotors() {
+        //Getting joystick inputs
+        double forward = -gamepad1.left_stick_y;
+        double rotate = gamepad1.right_stick_x;
+
+        //Calculate Power to wheels
+        double leftPower = (forward + rotate) * 0.9;
+        double rightPower = (forward - rotate) * 0.9;
+        double max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+        if (max > 1.0)
+        {
+            leftPower /= max;
+            rightPower /= max;
+        }
+
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+    }
+
+    private void controlHorizontalSliders() {
+        while (gamepad2.left_trigger > 0) {
+            double power = gamepad2.left_trigger;
+            leftSlider.setPower(power);
+            rightSlider.setPower(power);
+        }
+    }
+
+    private void controlVerticalSlider() {
+        while (gamepad2.right_trigger > 0) {
+            double power = gamepad2.left_trigger;
+            leftSlider.setPower(power);
+            rightSlider.setPower(power);
+        }
+    }
+
+    private void retractVerticalSlider() {
+        while (!((DcMotorEx) topSlider).isOverCurrent()) {
+            topSlider.setPower(1);
         }
     }
 }
